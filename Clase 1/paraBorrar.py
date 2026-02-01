6585ab6b60b45c7d62d337bb365c4a7c0c363fa1
edusalam <eduarfernando.salcedo@gmail.com>
2026-02-01 17:38:51 -0500

    gggggg

--- Clase 1/paraBorrar.py
+++ Clase 1/paraBorrar.py
@@ -1,7 +1,3 @@
 miPrimeAplicativo = "holamundo cruel mañana nos vemos"
 print(miPrimeAplicativo)
 
-pruebaBorrar = 'completare el curso de programacion en python'
-print (pruebaBorrar)
-
-pruebaculi = 'cara'
