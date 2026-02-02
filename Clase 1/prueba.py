732ac094bd469d93342c552ca4c71a24307fd9b2
edusalam <eduarfernando.salcedo@gmail.com>
2026-02-02 16:39:09 -0500

    actualizadodoeenero

---
+++ Clase 1/prueba.py
@@ -0,0 +1,20 @@
+'''
+var1 = 100
+var2 = 200
+
+R = var1 + var2
+
+print('la respuesta es:', R )
+
+a = 8
+b = 8
+c = a>=b
+print(c)
+'''
+edad = 52
+if edad >= 18:
+    print('es mayor de edad')
+elif edad <=10 and edad >=5 :
+    print('es un bebe')
+elif edad <= 1 and edad >5:
+    print('eres un recien nacido')
